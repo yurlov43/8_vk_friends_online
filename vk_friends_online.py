9baf8b3a4f1be9572615936a729b0b2ce62f8477
yurlov43 <yurlov43@yandex.ru>
2017-12-14 15:54:18 +0300

    A script tracking friends online in VK ready

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,29 +1,36 @@
+import getpass
 import vk
 
 
-APP_ID = -1  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
+APP_ID = 6298275
 
 
 def get_user_login():
-    pass
+    return input('Введите логин: ')
 
 
 def get_user_password():
-    pass
+    return getpass.getpass('Введите пароль: ')
 
 
 def get_online_friends(login, password):
     session = vk.AuthSession(
         app_id=APP_ID,
+        scope='friends',
         user_login=login,
         user_password=password,
     )
     api = vk.API(session)
-    # например, api.friends.get()
+    return api.friends.getOnline(online_mobile=1)
 
 
 def output_friends_to_console(friends_online):
-    pass
+    print('Друзья online с компьютера:')
+    for friend in friends_online['online']:
+        print(friend)
+    print('Друзья online с телефона:')
+    for friend in friends_online['online_mobile']:
+        print(friend)
 
 if __name__ == '__main__':
     login = get_user_login()
